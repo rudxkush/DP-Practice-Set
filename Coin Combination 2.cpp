b0512637a1ccf5505fc2b62748594b16c9a075a9
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-23 23:51:07 +0530

    Create Coin Combination 2.cpp

---
+++ Coin Combination 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int countAllSuchPossibilities(vector<int>& availableCoins, int desiredSum, int i) {
+    // Base Condition
+    if (desiredSum == 0) return 1;
+    if (desiredSum < 0) return 0;
+    if (i >= availableCoins.size()) return 0;
+
+    // Inclusion
+    int include = countAllSuchPossibilities(availableCoins, desiredSum - availableCoins[i], i);
+
+    // Exclusion
+    int exclude = countAllSuchPossibilities(availableCoins, desiredSum, i + 1);
+
+    // Return
+    return include + exclude;
+}
+
+int main() {int n;
+    cin >> n; // number of coins
+    vector<int> availableCoins(n);
+    for (int i = 0; i < n; i++) cin >> availableCoins[i];
+    
+    int desiredSum;
+    cin >> desiredSum;
+    cout << countAllSuchPossibilities(availableCoins, desiredSum,0) << endl;
+    return 0;
+}
