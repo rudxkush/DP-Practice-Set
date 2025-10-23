f61490297e92571e5c66b9812a9942c5c2ed296a
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-24 04:28:24 +0530

    Update and rename Coin Combination 2.cpp to Coin Combinations 2.cpp

--- Coin Combinations 2.cpp
+++ Coin Combinations 2.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int MOD = 1e9+7;
+vector<vector<int> > dp;
 int countAllSuchPossibilities(vector<int>& availableCoins, int desiredSum, int i) {
     // Base Condition
     if (desiredSum == 0) return 1;
-    if (desiredSum < 0) return 0;
     if (i >= availableCoins.size()) return 0;
 
+    // If already precomputed
+    if (dp[desiredSum][i] != -1) return dp[desiredSum][i];
+
     // Inclusion
-    int include = countAllSuchPossibilities(availableCoins, desiredSum - availableCoins[i], i);
+    int include = 0;
+    if (availableCoins[i] <= desiredSum)
+         include = countAllSuchPossibilities(availableCoins, desiredSum - availableCoins[i], i);
 
     // Exclusion
     int exclude = countAllSuchPossibilities(availableCoins, desiredSum, i + 1);
 
     // Return
-    return include + exclude;
+    return dp[desiredSum][i] = (include + exclude) % MOD;
 }
 
 int main() {
     int n;
-    cin >> n; // number of coins
+    int desiredSum;
+    cin >> n >> desiredSum;
     vector<int> availableCoins(n);
     for (int i = 0; i < n; i++) cin >> availableCoins[i];
-    
-    int desiredSum;
-    cin >> desiredSum;
-    sort(availableCoins.begin(), availableCoins.end());
+
+    dp.clear();
+    dp.resize(desiredSum + 1, vector<int>(n + 1, -1));
     cout << countAllSuchPossibilities(availableCoins, desiredSum,0) << endl;
     return 0;
 }
