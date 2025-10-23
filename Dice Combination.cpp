082c4a2bb89ac363ba239f304ace3e8c3a99d406
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-23 21:16:10 +0530

    Create Dice Combination.cpp

---
+++ Dice Combination.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD = 1e9+7;
+
+vector<int> dp;
+int countWaysToConstructSum(int currSum, int n) {
+  if(currSum > n) 
+    return 0;
+  
+  if(currSum == n) 
+    return 1;
+  
+  if(dp[currSum] != -1) 
+    return dp[currSum];
+  
+  
+  int count = 0;
+  for(int i = 1; i <= 6; i++) {
+    count = (count + countWaysToConstructSum(currSum + i, n)) % MOD;
+  }
+  
+  return dp[currSum] = count;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  dp.resize(n + 1, -1);
+  cout << countWaysToConstructSum(0, n) << endl;
+  return 0;
+}
